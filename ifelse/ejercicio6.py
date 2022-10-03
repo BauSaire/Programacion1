447969629adc12ca45e522130cd7054de31c1e6d
Bautista <sairebautista2@gmail.com>
2022-10-03 15:00:58 -0300

    Arreglo ejercicio 6

--- ifelse/ejercicio6.py
+++ ifelse/ejercicio6.py
@@ -4,11 +4,14 @@ cantidad es mayor a 700, incremente el precio en 5% para el exceso de Kw sobre
 700. Muestre el valor total a pagar.
 """
 kwConsumido = float(input("Ingrese la cantidad de KW consumido: "))
-preciokw = 4
-total = kwConsumido * preciokw
-
+preciokw = 8
 if kwConsumido > 700:
-    total += total * .05 
-    print("Monto total a pagar: ", str(total))
+    excedente =  kwConsumido - 700
+    kwConsumido -=  excedente
+    kwConsumido*= preciokw
+    excedente *= preciokw + preciokw*0.05
+    kwConsumido+= excedente 
+    print("Monto total a pagar: ", str(kwConsumido))
 else:
-       print("Monto total a pagar: ", str(total))
+    kwConsumido *=  preciokw        
+    print("Monto total a pagar: ", str(kwConsumido))
